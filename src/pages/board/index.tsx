d93df147547525cf010e53f34c9fd48bb7239410
kcleusio-dev <kcleusio.dev@gmail.com>
2024-11-11 16:07:38 +0100

    feat: obter dados do fireBase

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -2,24 +2,36 @@ import Head from "next/head";
 import { FormEvent, useState } from "react";
 import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/react";
-
+import Link from "next/link";
 import styles from './styles.module.scss';
 import { FiCalendar, FiClock, FiEdit2, FiPlus, FiTrash } from "react-icons/fi";
 import { SupportButton } from "@/components/SupportButton";
 import db from "@/utils/firestore";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs, where, query } from "firebase/firestore";
+import { format } from 'date-fns';
+
+type TaskList = {
+    id: string;
+    created: string | Date;
+    createdFormated?: string;
+    tarefa: string;
+    userId: string;
+    nome: string;
+}
 
 interface BoardProps {
     user: {
         id: string;
         nome: string;
     }
+    data: string;
 }
 
-export default function Board({ user }: BoardProps) {
+export default function Board({ user, data }: BoardProps) {
 
     const [input, setInput] = useState('');
-    
+    const [taskList, setTaskList] = useState<TaskList[]>(JSON.parse(data));
+
     async function handleAddTask(e: FormEvent) {
         e.preventDefault();
 
@@ -35,13 +47,22 @@ export default function Board({ user }: BoardProps) {
             nome: user.nome
         })
             .then((doc) => {
-                console.log('Registo realizado com sucesso!')
+                console.log('Registo realizado com sucesso!');
+                const data = {
+                    id: doc.id,
+                    created: new Date(),
+                    createdFormated: format(new Date(), 'dd MMM yyyy'),
+                    tarefa: input,
+                    userId: user.id,
+                    nome: user.nome
+                };
+
+                setTaskList([...taskList, data]);
+                setInput('');
             })
             .catch((err) => {
-                console.log('Erro ao registar', err);
+                console.log('Erro ao registar tarefa', err);
             })
-
-
     }
 
     return (
@@ -62,33 +83,42 @@ export default function Board({ user }: BoardProps) {
                     </button>
                 </form>
 
-                <h1>Você tem 2 tarefas</h1>
+                <h1>Você tem {taskList.length} {taskList.length === 1 ? 'Tarefa' : 'Tarefas'}</h1>
 
                 <section>
-                    <article className={styles.taskList}>
-                        <p>Aprender a criar projectos com Next JS e aplicando banckend Firebase</p>
-                        <div className={styles.actions}>
-                            <div>
+                    {taskList && taskList.map(task => (
+
+                        < article key={task.id}
+                            className={styles.taskList}
+                        >
+
+                            <Link href={`/board/${task.id}`}>
+                                <p>{task.tarefa}</p>
+                            </Link>
+                            <div className={styles.actions}>
                                 <div>
-                                    <FiCalendar size={20} color="#FFB800" />
-                                    <time>07 de Julho de 2024</time>
+                                    <div>
+                                        <FiCalendar size={20} color="#FFB800" />
+                                        <time>{task.createdFormated}</time>
+                                    </div>
+                                    <button>
+                                        <FiEdit2 size={20} color="#FFF" />
+                                        <span>Editar</span>
+                                    </button>
                                 </div>
+
                                 <button>
-                                    <FiEdit2 size={20} color="#FFF" />
-                                    <span>Editar</span>
+                                    <FiTrash size={20} color="#FF3636" />
+                                    <span>Excluir</span>
                                 </button>
                             </div>
 
-                            <button>
-                                <FiTrash size={20} color="#FF3636" />
-                                <span>Excluir</span>
-                            </button>
-                        </div>
+                        </article>
+                    ))}
 
-                    </article>
                 </section>
 
-            </main>
+            </main >
 
             <div className={styles.vipContanier}>
                 <h3>Obrigado por apoiar nosso projecto.</h3>
@@ -121,6 +151,24 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
         }
     }
 
+    //pegar tarefas from firebase
+    /*
+    const tasks = await firebase.firestore().collection('tarefas')
+    .where('userId','==',session?.token.sub)
+    .orderBy('created', 'asc').get();
+    */
+
+    const task = query(collection(db, 'tarefas'), where('userId', '==', session?.token.sub));
+
+    const tasks = await getDocs(task);
+    const data = JSON.stringify(tasks.docs.map(u => {
+        return {
+            id: u.id,
+            createdFormated: format(u.data().created.toDate(), 'dd MMM yyyy'),
+            ...u.data(),
+        }
+    }))
+
     const user = {
         nome: session?.token.name ?? null,
         id: session?.token.sub ?? null
@@ -128,7 +176,8 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
 
     return {
         props: {
-            user
+            user,
+            data
         }
     }
 
